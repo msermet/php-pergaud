9aba092673ccb06890bc0dd98522549dc065bf6c
Maxime Sermet <maximesermet075@gmail.com>
2023-12-19 08:54:17 +0100

    quizz php

---
+++ projets/php-tp/sequence6/test-fonctions-quizz-partie1.php
@@ -0,0 +1,20 @@
+<?php
+
+$questions=[
+    [
+        'question' => "Quelle est la capitale de la France ?",
+        'reponses' => "1. Paris","2. Berlin","3. Rome","4. Madrid",
+        'correct' => 1
+    ],
+    [
+        'question' => "Quel est le résultat de 3+4 ?",
+        'reponses' => "1. 5","2. 7","3. 9","4. 12",
+        'correct' => 2
+    ],
+    [
+        'question' => "Quel est le résultat de 3+4 ? ?",
+        'reponses' => "1. 5","2. 7","3. 9","4. 12",
+        'correct' => 2
+    ],
+
+];
