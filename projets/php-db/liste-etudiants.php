4bc46d5f370ffecf8b2bfcf531367769d25885c4
Maxime Sermet <maximesermet075@gmail.com>
2024-01-10 11:54:11 +0100

    sql

---
+++ projets/php-db/liste-etudiants.php
@@ -0,0 +1,10 @@
+<?php
+
+// Définir les informations de connexion
+const DB_HOST = "localhost:3306";
+const DB_NAME = "db_intro";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+
+// Utiliser PDO pour créer une connexion à la DB (base de données)
+$connexion =
