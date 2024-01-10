291b21381931fc965d40b344d6b492843b6e02a5
Maxime Sermet <maximesermet075@gmail.com>
2024-01-10 17:53:30 +0100

    sql

--- projets/php-db/liste-etudiants.php
+++ projets/php-db/liste-etudiants.php
@@ -7,4 +7,19 @@ const DB_USER = "root";
 const DB_PASSWORD = "";
 
 // Utiliser PDO pour créer une connexion à la DB (base de données)
-$connexion =
+$connexion = new PDO(dsn:"mysql:host=".DB_HOST.";dbname=".DB_NAME,username:DB_USER,password:DB_PASSWORD);
+
+// 1. Exécution de la requête
+// 1.1 Préparation de la requête
+$requete=$connexion->prepare("SELECT prenom_etudiant,nom_etudiant FROM etudiant ORDER BY nom_etudiant");
+// 1.2 Exécution de la requête (envoie de la requête)
+$requete->execute();
+// 2. Récupération des enregistrements
+//$requete->setFetchMode();
+$resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
+// 3. Affichage des résultats
+foreach ($resultats as $resultat) {
+    // $resultat : tableau associatif représentant un enregistrement
+    echo $resultat["prenom_etudiant"]." ".
+        $resultat["nom_etudiant"].PHP_EOL;
+}
